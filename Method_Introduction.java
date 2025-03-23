f84c9e4bb36d19714756f6825e249e9c49cf0557
other works <ishitagupta2211@gmail.com>
2025-03-23 21:30:11 +0530

    Day 2

---
+++ Method_Introduction.java
@@ -0,0 +1,15 @@
+package JavaTraining;
+
+public class Method_Introduction {
+
+	public static void main(String[] args) {
+		//No argument method
+		add();
+	}
+    public static void add() 
+    {
+    int num1=20;
+    int num2=30;
+    System.out.println(num1+num2);
+    
+}}
