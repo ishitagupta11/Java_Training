7992a655089b68a84820bf5ee46f3b3cfcc295cf
other works <ishitagupta2211@gmail.com>
2025-03-23 21:24:23 +0530

    Day 2

---
+++ Ternary_Operator.java
@@ -0,0 +1,27 @@
+package JavaTraining;
+
+public class Ternary_Operator {
+
+	public static void main(String[] args) {
+		//Comparing three number and find the greatest
+		int num1=90; //10
+		int num2=30; //20
+		int num3=60; //40
+        String maxi=(num1>num2)?(num1>num3?num1+" is greatest":num3 + " is greatest"):num2>num3?num2+ " is greatest":num3+ " is greatest";
+        System.out.println(maxi);
+		                		
+		
+		
+	
+		
+		
+		
+		
+		
+		
+	    //int maximum=n1>n2?n1>n3?n1:n3:n2>n3?n2:n3;
+	}
+	}
+
+
+
