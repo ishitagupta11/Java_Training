cf1de42d72aca3b1421a37a826cfcf3d629f6d3b
other works <ishitagupta2211@gmail.com>
2025-03-23 20:33:10 +0530

    Day 2

---
+++ Increment_Operator.java
@@ -0,0 +1,19 @@
+package JavaTraining;
+
+public class Increment_Operator {
+
+	public static void main(String[] args) {
+		int a=5,b=5,c=5;
+		int exp=a+b++ + ++c;
+		int d=10;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(exp);
+		System.out.println(++d);
+		
+		
+		
+	}
+
+}
