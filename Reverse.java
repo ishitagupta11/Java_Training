db9ab5fa9c0d8e4c8b4eb160edf22f6e54a6110b
other works <ishitagupta2211@gmail.com>
2025-03-23 17:29:39 +0530

    Day 1

---
+++ Reverse.java
@@ -0,0 +1,15 @@
+package JavaTraining;
+
+public class Reverse {
+
+	public static void main(String[] args) {
+		int num=123;
+		System.out.print(num%10); //3
+		num=num/10; //12
+		System.out.print(num%10); //2
+		System.out.print(num/10); //1
+    	
+    	
+	}
+
+}
